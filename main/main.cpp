4bd7dcec36a95b8ed0cd8eb6b9e0ab0547b8df51
huanghan0912 <2235812260@qq.com>
2022-12-12 14:55:59 +0800

    建立

---
+++ main/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "lvgl.h"
+#include "Screen.h"
+
+Screen screen;
+
+extern "C" void app_main(void)
+{
+
+}
